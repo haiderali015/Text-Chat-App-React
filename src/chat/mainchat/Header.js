6d3412ced6bfae9cfd3a6910e5493f57f1cd99f7
haiderali015 <ha347278@gmail.com>
2025-04-03 23:16:07 +0500

    chat page setup

--- src/chat/mainchat/Header.js
+++ src/chat/mainchat/Header.js
@@ -4,21 +4,33 @@ import CardHeader from '@mui/material/CardHeader';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-import { Typography } from '@mui/material';
-
+import { Button, Typography } from '@mui/material';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import CallIcon from '@mui/icons-material/Call';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 const Header = () => {
   return (
-    <Card sx={{ bgcolor: "primary.light", color: "primary.contrastText", borderRadius: 0 }}>
+    <Card sx={{borderRadius: 0}}>
       <CardHeader
         avatar={
+        <>
+            <Button sx={{minWidth:'auto' , mr:1}}>
+                <ArrowBackIcon/>
+            </Button>
           <Avatar>
             R
           </Avatar>
+        </>
         }
         action={
-          <IconButton aria-label="settings" sx={{color: "primary.contrastText"}}>
-            Back
+        <>
+          <IconButton>
+            <VideoCallIcon/>
           </IconButton>
+          <IconButton>
+            <CallIcon/>
+          </IconButton>
+        </>
         }
         title="Haider"
         subheader={<Typography variant='caption'>Developer</Typography>}
