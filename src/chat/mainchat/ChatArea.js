771a9119d5807d54b2c595308879bbbd2a1284b8
haiderali015 <ha347278@gmail.com>
2025-04-08 00:19:24 +0500

    chat area created

--- src/chat/mainchat/ChatArea.js
+++ src/chat/mainchat/ChatArea.js
@@ -1,25 +1,60 @@
 import React from 'react'
-import { Avatar, Box, Chip, ListItem, ListItemAvatar, ListItemText, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Chip, IconButton, List, ListItem, ListItemAvatar, ListItemText, Paper, Stack, Typography } from "@mui/material";
+import ReplyIcon from '@mui/icons-material/Reply';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 const ChatArea = () => {
   return (
     <Box sx={{height:'100%',  flex:'1 0 0', background:'#f9f9f9', overflowY:'auto'}}>
-        <Stack direction="row" sx={{justifyContent:'center'}}>
+        <Stack direction="row" sx={{justifyContent:'center', py:2, position:'sticky', top:0 ,zIndex:2 ,background:'f9f9f9'}}>
           <Chip label="Today" />
         </Stack>
-
-      <ListItem alignItems="flex-start">
+      <List sx={{p:0, overflowY:"auto", flex:"1 0 0"}}>
+        <ListItem alignItems="flex-start" sx={{mb:2}}>
+          <Box sx={{display:'flex', width:"80%"}}>
             <ListItemAvatar>
               <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             </ListItemAvatar>
-            <ListItemText
-              primary="Haider Ali"
+            <Paper sx={{width: '100%', p:1.5}}>
+              <ListItemText sx={{m:0, color:'black'}} primary="Zainab"
               secondary={
                 <Typography  variant="caption" sx={{ color: 'text.primary'}}>
-                  Ali Connors
+                  hello kaise ho ?
                 </Typography>
               }
             />
+            </Paper>
+          </Box>  
         </ListItem>
+        
+        <ListItem alignItems="flex-start" sx={{flexDirection:'row-reverse', mb:2}}>
+          <Box sx={{display:'flex', width:"80%", flexDirection:'row-reverse'}}>
+            <ListItemAvatar sx={{display:'flex', flexDirection:'row-reverse'}}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <Paper sx={{width: '100%', p:1.5, bgcolor:'#ccc', color:'primary.contrastText'}}>
+              <ListItemText sx={{m:0, color:'black'}} primary="Haider Ali"
+              secondary={
+                <Typography  variant="caption" sx={{ color: 'text.primary'}}>
+                  men thek ap sunao
+                </Typography>
+              }
+            />
+            <Box sx={{mt:1, color:'black', display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+                <Typography variant='body2'>12:30 PM</Typography>
+                <Box>
+                  <IconButton size='small'>
+                    <ReplyIcon fontSize='small'/>
+                  </IconButton>
+                  <IconButton size='small' color='error'>
+                    <DeleteIcon fontSize='small'/>
+                  </IconButton>
+                </Box>
+            </Box>
+            </Paper>
+          </Box>  
+        </ListItem>
+      </List>
     </Box>
   )
 }
--- src/chat/profile/index.js
+++ src/chat/profile/index.js
@@ -1,8 +1,15 @@
+import {Avatar, Box, Typography} from "@mui/material"
 const Profile = () =>{
     return(
-        <div>
-            <span>Profile</span>
-        </div>
+        <Box sx={{background:"#eee", width:"25vw", display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column'}}>
+            <Avatar src="/assets/haider.PNG" sx={{width:'156px', height:'156px'}}></Avatar>
+            <Typography variant="h4" sx={{textTransform:'uppercase', color:'primary.light', mt:3}}>
+                Haider Ali
+            </Typography>
+            <Typography variant="subtitle1">Developer</Typography>
+            <Typography variant="body2">a@g.com</Typography>
+
+        </Box>
     )
 }
 export default Profile
